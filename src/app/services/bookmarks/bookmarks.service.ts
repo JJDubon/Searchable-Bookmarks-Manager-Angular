739c954ff6fbb3a099cee548d475f02f9906f2b0
Jeremy Dubon <JeremyDubon@gmail.com>
2020-07-31 20:07:55 -0400

    Moves a bookmark's children in response to api events

--- src/app/services/bookmarks/bookmarks.service.ts
+++ src/app/services/bookmarks/bookmarks.service.ts
@@ -20,54 +20,7 @@ export class BookmarksService {
 
     // When the tree has been read, listen to the bookmark api's change events
     this.initialized$.subscribe(() => {
-
-      // Add a bookmark to the map when a bookmark is created
-      this.chromeExtensionBridge.onBookmarkCreated$.subscribe(({model}) => {  
-
-        // Add the bookmark to the map
-        this.bookmarksMap[model.id] = model
-        
-        // Fetch the updated order of the new bookmark's parent
-        const parent = this.bookmarksMap[model.parentId] as BookmarkFolderModel;
-        if (parent) {
-          this.chromeExtensionBridge.getChildrenIds(model.parentId).subscribe(ids => {
-            parent.children = ids;
-            this.bookmarkChanged$.next(parent);
-          });
-        }
-  
-      });
-
-      // Remove the bookmark when one has been deleted
-      this.chromeExtensionBridge.onBookmarkRemoved$.subscribe(({id, parentId}) => {
-
-        // Remove the bookmark from the map
-        delete this.bookmarksMap[id];
-
-        // Remove the bookmark from its parent's "children" member
-        const parent = this.bookmarksMap[parentId] as BookmarkFolderModel;
-        if (parent) {
-          parent.children = parent.children.filter(x => x !== id);
-          this.bookmarkChanged$.next(parent);
-        }
-
-      });
-
-      // Update the "title" and "url" field in response to the api's change event
-      this.chromeExtensionBridge.onBookmarkChanged$.subscribe(({id, title, url}) => {
-
-        const bookmark = this.bookmarksMap[id];
-        if (bookmark instanceof BookmarkLinkModel) {
-          bookmark.title = title;
-          bookmark.url = url;
-        } else {
-          bookmark.title = title;
-        }
-
-        this.bookmarkChanged$.next(bookmark);
-
-      });
-
+      this.createListeners();
     });
 
     // Retrieve the bookmarks tree from the chrome extension api
@@ -93,6 +46,89 @@ export class BookmarksService {
 
   public getBookmark(id: string): BookmarkBaseModel {
     return this.bookmarksMap[id];
+  }
+
+  private createListeners(): void {
+
+    // Add a bookmark to the map when a bookmark is created
+    this.chromeExtensionBridge.onBookmarkCreated$.subscribe(({model}) => {  
+
+      // Add the bookmark to the map
+      this.bookmarksMap[model.id] = model
+      
+      // Fetch the updated order of the new bookmark's parent
+      const parent = this.bookmarksMap[model.parentId] as BookmarkFolderModel;
+      if (parent) {
+        this.chromeExtensionBridge.getChildrenIds(model.parentId).subscribe(ids => {
+          parent.children = ids;
+          this.bookmarkChanged$.next(parent);
+        });
+      }
+
+    });
+
+    // Remove the bookmark when one has been deleted
+    this.chromeExtensionBridge.onBookmarkRemoved$.subscribe(({id, parentId}) => {
+
+      // Remove the bookmark from the map
+      delete this.bookmarksMap[id];
+
+      // Remove the bookmark from its parent's "children" member
+      const parent = this.bookmarksMap[parentId] as BookmarkFolderModel;
+      if (parent) {
+        parent.children = parent.children.filter(x => x !== id);
+        this.bookmarkChanged$.next(parent);
+      }
+
+    });
+
+    // Update the "title" and "url" field in response to the api's change event
+    this.chromeExtensionBridge.onBookmarkChanged$.subscribe(({id, title, url}) => {
+
+      const bookmark = this.bookmarksMap[id];
+      if (bookmark instanceof BookmarkLinkModel) {
+        bookmark.title = title;
+        bookmark.url = url;
+      } else {
+        bookmark.title = title;
+      }
+
+      this.bookmarkChanged$.next(bookmark);
+
+    });
+
+    // Update the order of a bookmarks children when moved
+    this.chromeExtensionBridge.onBookmarkMoved$.subscribe(({id, parentId, oldParentId, index, oldIndex}) => {
+
+      const parent = this.bookmarksMap[parentId] as BookmarkFolderModel;
+      if (parent) {
+        this.chromeExtensionBridge.getChildrenIds(parentId).subscribe(ids => {
+          parent.children = ids;
+          this.bookmarkChanged$.next(parent);
+        });
+      }
+
+      const oldParent = this.bookmarksMap[oldParentId] as BookmarkFolderModel;
+      if (oldParent) {
+        this.chromeExtensionBridge.getChildrenIds(oldParentId).subscribe(ids => {
+          oldParent.children = ids;
+          this.bookmarkChanged$.next(oldParent);
+        });
+      }
+
+    });
+
+    // Updates a bookmark's children when reordered
+    this.chromeExtensionBridge.onBookmarkChildrenReordered$.subscribe(({id, childIds}) => {
+
+      const bookmark = this.bookmarksMap[id] as BookmarkFolderModel;
+      if (bookmark) {
+        bookmark.children = childIds;
+        this.bookmarkChanged$.next(bookmark);
+      }
+
+    });
+    
   }
 
 }
