fb4dcc9ed6d0cc0dbf6eaaa1bf6bd963542db017
Jeremy Dubon <JeremyDubon@gmail.com>
2020-09-12 21:35:32 -0400

    Adds in tests for the AllowUnsafePipe class

--- src/app/pipes/allow-unsafe.pipe.spec.ts
+++ src/app/pipes/allow-unsafe.pipe.spec.ts
@@ -1,8 +1,40 @@
+import { async, TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 import { AllowUnsafePipe } from './allow-unsafe.pipe';
 
 describe('AllowUnsafePipe', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AllowUnsafePipe ],
+      providers: [ 
+        AllowUnsafePipe,
+        { 
+          provide: DomSanitizer, 
+          useValue: { bypassSecurityTrustUrl() { } }
+        }
+      ]
+    });
+  }));
+
   it('create an instance', () => {
-    const pipe = new AllowUnsafePipe();
+
+    const pipe = TestBed.inject(AllowUnsafePipe);
     expect(pipe).toBeTruthy();
+
   });
+
+  it('calls the dom sanitizer', () => {
+
+    const testValue = 'test value';
+    const domSanitizer = TestBed.inject(DomSanitizer);
+    const spy = spyOn(domSanitizer, 'bypassSecurityTrustUrl').and.returnValue(testValue);
+
+    const pipe = TestBed.inject(AllowUnsafePipe);
+    const result = pipe.transform(testValue);
+    expect(domSanitizer.bypassSecurityTrustUrl).toHaveBeenCalled();
+    expect(result).toBe(testValue);
+
+  });
+
 });
